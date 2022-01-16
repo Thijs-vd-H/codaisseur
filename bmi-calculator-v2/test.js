975ffc8afda1202ec7deb869ce99e7a2cf5acb8c
Thijs-vd-H <85699468+Thijs-vd-H@users.noreply.github.com>
2022-01-16 20:00:59 +0100

    Add files via upload

---
+++ bmi-calculator-v2/test.js
@@ -0,0 +1,8 @@
+let whatQuestion = [
+    'This is your',
+    'last',
+    'question'
+];
+console.log(whatQuestion[0]);
+console.log(whatQuestion[1]);
+console.log(whatQuestion[2]);
---
+++ static-html-website/helloworld.js
@@ -0,0 +1,14 @@
+// helloworld.js
+console.log("Hello World!");
+
+const weightInKg = 82;
+
+// We console.log the variable weightInKg which refers to the number value 82
+console.log(weightInKg); // 82
+
+
+
+
+
+const weightInKg = 82;
+const heightInM = 1.79;
